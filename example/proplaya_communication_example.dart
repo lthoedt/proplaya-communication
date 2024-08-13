7da64e3e496d67cccfc8b624a5f0ab51528101cc
lthoedt <lthoedt@gmail.com>
2024-08-13 14:42:53 +0200

    init package

---
+++ example/proplaya_communication_example.dart
@@ -0,0 +1,6 @@
+import 'package:proplaya_communication/proplaya_communication.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/proplaya_communication_test.dart
@@ -0,0 +1,16 @@
+import 'package:proplaya_communication/proplaya_communication.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
